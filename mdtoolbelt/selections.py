94452ff7789ba464540d4d736ac3cf75759b3061
Dani Beltrán <daniel.beltran@irbbarcelona.org>
2022-02-09 17:13:02 +0100

    new selection exported formats

--- mdtoolbelt/selections.py
+++ mdtoolbelt/selections.py
@@ -14,6 +14,25 @@ class Selection:
         indexes = [ atom.getIndex() for atom in prody_selection.iterAtoms() ]
         return cls(indexes)
 
+    def to_prody (self) -> str:
+        return 'index ' + ' '.join([ str(index) for index in self.atom_indices ])
+
+    def to_mdanalysis (self) -> str:
+        return self.to_prody() # Same notation than prody for atom indices
+
+    def to_pytraj (self) -> str:
+        # NEVER FORGET: Pytraj counts atoms starting at 1, not at 0
+        return '@' + ','.join([ str(index+1) for index in self.atom_indices ])
+
+    # Get a string made of all indexes separated by underscores
+    # This string can be then passed as a bash argument and easily parsed by other programms
+    # Indices can start from 0 or from 1
+    def to_bash (self, one_start : bool = False) -> str:
+        if one_start:
+            return '_'.join([ str(index + 1) for index in self.atom_indices ])
+        else:
+            return '_'.join([ str(index) for index in self.atom_indices ])
+
     # Generate a vmd selection in tcl format
     def to_vmd (self) -> str:
         return 'index ' + ' '.join([ str(index) for index in self.atom_indices ])
@@ -33,4 +52,5 @@ class Selection:
             # Atom indices go from 0 to n-1
             # Add +1 to the index since gromacs counts from 1 to n
             content += str(index + 1) + ' '
+        content += '\n' 
         return content
