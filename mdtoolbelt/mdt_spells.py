49243b98c107def950b688d12c76826eecbbf11d
Dani Beltrán <daniel.beltran@irbbarcelona.org>
2022-02-28 17:25:27 +0100

    added new function to get trajectory subsets

--- mdtoolbelt/mdt_spells.py
+++ mdtoolbelt/mdt_spells.py
@@ -1,5 +1,6 @@
 import os
 from subprocess import run, PIPE
+import mdtraj as md
 
 # Multiple files may be selected with bash syntax (e.g. *.dcd)
 # Tested supported input formats are .dcd
@@ -32,4 +33,56 @@ convert_traj.format_sets = [
             'output_trajectory_filename': {'dcd', 'xtc', 'trr', 'nc', 'h5', 'binpos'}
         }
     },
+]
+
+# Get specific frames from a trajectory
+def get_trajectory_subset (
+    input_structure_filename : str,
+    input_trajectory_filename : str,
+    output_trajectory_filename : str,
+    start : int = 0,
+    end : int = 0,
+    step : int = 1
+):
+    # In case no end is passed return the only the start frame
+    if not end:
+        end = start + 1
+
+    # Load the trajectory frame by frame and get only the desired frames
+    trajectory = md.iterload(input_trajectory_filename, top=input_structure_filename, chunk=1)
+    # Get the first chunk
+    reduced_trajectory = None
+    for i, chunk in enumerate(trajectory):
+        if i == start:
+            reduced_trajectory = chunk
+            break
+    # Get further chunks
+    relative_end = end - start
+    for i, chunk in enumerate(trajectory, 1): # Start the count at 1
+        if i == relative_end:
+            break
+        if i % step == 0:
+            reduced_trajectory = md.join([reduced_trajectory, chunk], check_topology=False)
+
+    # Write reduced trajectory to output file
+    reduced_trajectory.save(output_trajectory_filename)
+get_trajectory_subset.format_sets = [
+    {
+        'inputs': {
+            'input_structure_filename': {'pdb', 'h5'},
+            'input_trajectory_filename': {'dcd', 'xtc', 'trr', 'nc', 'binpos'}
+        },
+        'outputs': {
+            'output_trajectory_filename': {'dcd', 'xtc', 'trr', 'nc', 'binpos'}
+        }
+    },
+    {
+        'inputs': {
+            'input_structure_filename': None,
+            'input_trajectory_filename': {'pdb', 'h5'}
+        },
+        'outputs': {
+            'output_trajectory_filename': {'pdb', 'h5'}
+        }
+    }
 ]
